1e489d9e29bdf1d3008f15608363e8c9017c0f7b
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-11-01 10:39:13 +0000

    Week 4 Project structure Setup

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace RAD3012122Week4.ClubDomain.Classes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ClubDomain.Classes.ClubModels.ClubsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ClubDomain.Classes.ClubModels.ClubsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
