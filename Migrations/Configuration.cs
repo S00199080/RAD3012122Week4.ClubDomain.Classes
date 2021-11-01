9a40a35a8a5ec5fd30ed60dbc559313149f86c21
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-11-01 11:32:41 +0000

    Enabled Migrations

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -4,7 +4,6 @@
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    
 
     internal sealed class Configuration : DbMigrationsConfiguration<ClubDomain.Classes.ClubModels.ClubsContext>
     {
