5ccb66749b3e2bc1ec4001ffd733d55722250768
S00199080 <S00199080@LAB-D2039-24>
2021-11-01 09:37:08 +0000

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RAD3012122Week4;
+
+namespace RAD3012122Week4.ClubDomain.Classes
+{
+    public class Class1
+    {
+
+    }
+}
